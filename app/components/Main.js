e675df009445c2415c6f578a25f132a23ecd0a52
parshencev <parshencev.vlad@gmail.com>
2019-02-17 15:24:27 +0300

    write integration ffmpeg

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,25 +4,85 @@ import { Link } from 'react-router-dom';
 import styles from './Main.scss';
 import File from './UI/File';
 import Header from './UI/Header';
+import Cornvector from './../libs/ffmpeg.js';
 // const fixPath = require('fix-path');
 // fixPath();
 
+class FFmpeg {
+  constructor(){
+    setTimeout(() => {
+      this.name = "hue " + Math.random()*100;
+      this.oninfo();
+    }, 1000);
+    setTimeout(() => {
+      let value = Math.random()*100;
+      this.onprogress({
+        value
+      });
+    }, 2000);
+  }
+}
 
 export default class Home extends Component<Props> {
   props: Props;
+
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleInfo = this.handleInfo.bind(this);
+  }
 
   state = {
     files: []
   }
 
+  handleInfo(){
+    let files = this.state.files;
+
+    this.setState({ files });
+  }
+
+  handleProgress(key, progressObject){
+    let files = this.state.files,
+        file = files[key];
+
+    file.progress = progressObject.value;
+
+    this.setState({
+      files
+    });
+  }
+
+  handleError(key, error){
+
+  }
+
+  handleDone(key, doneObject){
+
+  }
+
   handleChange(e){
-    console.log(e.target.files);
-    this.setState({files: [{ name: e.target.files[0].name }]});
-   // this.setState({
-   //    files: e.target.files.map(f => {
-   //      name = f.name;
-   //    })
-   //  });
+    let files = e.target.files,
+        filesLength = files.length,
+        filesArray = this.state.files;
+
+    for (let i = 0; i < filesLength; i++) {
+      let file = files[i],
+          fileObject = new Cornvector(file.path),
+          fileKey = filesArray.push(fileObject) - 1;
+
+      fileObject.key = fileKey;
+      fileObject.name = file.name;
+
+      fileObject.oninfo = this.handleInfo;
+      fileObject.onprogress = this.handleProgress.bind(this, fileKey);
+      fileObject.onerror = this.handleError.bind(this, fileKey);
+      fileObject.ondone = this.handleDone.bind(this, fileKey);
+    }
+
+    this.setState({
+      files: filesArray
+    });
   }
 
   render() {
@@ -37,7 +97,7 @@ export default class Home extends Component<Props> {
             <File name={file.name} />
           ))}
         </div>
-        <input type="file" onChange={this.handleChange.bind(this)}/>
+        <input type="file" multiple onChange={this.handleChange}/>
       </div>
     );
   }
